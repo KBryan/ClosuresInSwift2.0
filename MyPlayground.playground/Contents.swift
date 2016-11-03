b54a347edbf909c993b8c97d8f10f321c4beb833
Kwame Bryan <kwame.bryan@gmail.com>
2016-11-03 15:41:32 -0400

    Updated for Swift 3.0

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 // Definition of protocols and generics with extensions
 protocol Addable {
-    func +(lhs:Self, rhs:Self) -> Self
+    static func +(lhs:Self, rhs:Self) -> Self
 }
 func add<T:Addable>(a:T,b:T) -> T {
     return a + b
@@ -13,9 +13,9 @@ extension String:Addable {}
 extension Int:Addable {}
 extension Double:Addable {}
 
-add(5, b: 5)
-add("Kwame", b: "Bryan")
-add(2.5, b: 4.5)
+add(a: 5, b: 5)
+add(a: "Kwame", b: "Bryan")
+add(a: 2.5, b: 4.5)
 /// functions as arguments
 func operate(x:Int,y:Int, fn:(Int,Int)->()){
     fn(x,y)
@@ -23,26 +23,24 @@ func operate(x:Int,y:Int, fn:(Int,Int)->()){
 func add(x:Int,y:Int) {
     print(x+y)
 }
-operate(1, y: 3, fn: add)
+operate(x: 1, y: 3, fn: add)
 // Tuple example
 func returnSomeIntegers(a:Int,b:Int) -> (Int,Int) {
     return (a,b)
 }
-var returnMultipleValues = returnSomeIntegers(8, b: 4)
+var returnMultipleValues = returnSomeIntegers(a: 8, b: 4)
 returnMultipleValues.0
 returnMultipleValues.1
 /// curried functions
-func curriedExample(a:Int)(b:Int)(c:Int) -> Int {
+func curriedExample(a:Int, b:Int, c:Int) -> Int {
     return a+b+c
 }
-curriedExample(2)(b:3)(c:4)
+curriedExample(a: 4, b: 5, c: 5)
 /// passing closures to functions
 func operate(a:Int,b:Int, fn:(Int,Int) ->Int) {
     fn(a,b)
 }
-operate(1, y: 2) { (a, b) -> () in
-    return a+b
-}
-operate(1, y: 4) { (a,b) in a+b }
-operate(1, y: 5) { $0+$1 }
+
+operate(x:1, y: 4) { (a,b) in a + b }
+operate(x: 1, y: 5) { $0 + $1 }
 
